2467fb5961c6f26233198bf2605b5ad7a4309c3c
Andrew Cruz <ajdat10@gmail.com>
2021-12-14 03:27:55 -0500

    create and delete comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -155,7 +155,8 @@ router.put('/unlike/:id', auth, async (req, res) => {
 router.post(
   "/comment/:id",
     [auth, [
-        check("text", "Text is required").not().isEmpty()]],
+        check("text", "Text is required").not().isEmpty()
+    ]],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -186,5 +187,41 @@ router.post(
   }
 );
   
+//@route   DELETE api/posts/comment/:id/:comment_id
+//@desc    Delete comment
+//@access  Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
 
+        // Pull out comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id)
+        
+        // Make sure comment exists
+        if (!comment) {
+            return res.status(404).json({ msg: 'Comment does not exist' })
+            
+        }
+
+        // Check user
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' })
+        }
+
+        // Get remove index
+        const removeIndex = post.comments
+          .map(comment => comment.user.toString())
+          .indexOf(req.user.id);
+
+        post.comments.splice(removeIndex, 1);
+
+        await post.save();
+
+        res.json(post.comments);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+} );
 module.exports = router;
